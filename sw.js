6e1dc91386336e9d143a232bd44f22c15c0ae05f
Barbara Utsalo <emiutsalo@gmail.com>
2019-08-04 00:05:50 -0400

    Used the right fetch function

--- sw.js
+++ sw.js
@@ -49,7 +49,19 @@ self.addEventListener('activate', function(event) {
 self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
+            if(response !== undefined){
+                return response;
+            }
+            else{
+                return fetch(event.request).then(function(response){
+                    var responseToCache = response.clone();
+
+                    caches.open(staticCacheName).then(function(cache){
+                        cache.put(event.reqest, responseToCache);
+                    });
+                    return response;
+                });
+            };
+        });
     );
 });
