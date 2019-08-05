e7bda9dc2cf2442b4991cbde19ef3cfa2f5331a9
Barbara Utsalo <emiutsalo@gmail.com>
2019-08-04 22:20:30 -0400

    Fixed service worker for fetch function

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
-// install Service worker
-let staticCacheName = 'sw-restaurant-cache';
+let staticCacheName = 'sw-restaurant-cache-v1';
 self.addEventListener("install", function(event){
     event.waitUntil(
       caches.open(staticCacheName).then(function(cache){
+          console.log('Opened Cache');
           return cache.addAll([
               './index.html',
               './restaurant.html',
-              './css/styles.css',
-              './css/responsive_screen.css',
-              './js/dbhelper.js',
-              './js/main.js',
-              './js/restaurant_info.js',
+            //   './css/styles.css',
+            //   './css/responsive_screen.css',
+            //   './js/dbhelper.js',
+            //   './js/main.js',
+            //   './js/restaurant_info.js',
               './img/1.jpg',
               './img/2.jpg',
               './img/3.jpg',
@@ -21,7 +21,7 @@ self.addEventListener("install", function(event){
               './img/8.jpg',
               './img/9.jpg',
               './img/10.jpg',
-              './data/restaurants.json',
+            //   './data/restaurants.json'
           ]);
       })  
     );
@@ -32,8 +32,9 @@ self.addEventListener("install", function(event){
 self.addEventListener('activate', function(event) {
     event.waitUntil(
         caches.keys().then(function(cacheNames) {
+            console.log("Cache ACtivated ", cacheNames);
             return Promise.all(
-                cacheNames.filter(function(cacheName) {
+                cacheNames.map(function(cacheName) {
                     return cacheName.startsWith('restaurant-') &&
                         cacheName != staticCacheName;
                 }).map(function(cacheName) {
@@ -48,17 +49,18 @@ self.addEventListener('activate', function(event) {
 self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request).then(function(response) {
-            if(response !== undefined){
+            console.log("Responded", response)
+            if(response){
                 return response;
-            }
-            else{
+            } else{
+                console.log('event '+ event.request.url)
                 return fetch(event.request).then(function(response){
-                    var responseToCache = response.clone();
-
-                    caches.open(staticCacheName).then(function(cache){
-                        cache.put(event.reqest, responseToCache);
-                    });
-                    return response;
+                    // var responseToCache = response.clone();
+                    return caches.open(staticCacheName).then(function(cache){
+                        cache.put(event.request, response.clone());
+                        return response;
+                    }).catch(error => console.log('Error', error))
+                    // return response;
                 });
             };
         })
