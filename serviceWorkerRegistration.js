8bb33595b00a5f2584c77b0b6b77551071cd2840
Barbara Utsalo <emiutsalo@gmail.com>
2019-08-04 22:21:46 -0400

    Perform Code CleanUp

--- serviceWorkerRegistration.js
+++ serviceWorkerRegistration.js
@@ -1,11 +1,13 @@
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js', { scope: '' })
-    .then(function(reg) {
-     // registration passed
-     console.log('Registration was successful. Scope is ' + reg.scope);
-     }).catch(function(error) {
-     // registration failed
-     console.log('Registration failed with ' + error);
-     });
+    window.addEventListener('load', function(){
+      navigator.serviceWorker.register('./sw.js', { scope: '' })
+      .then(function(reg) {
+       // registration passed
+       console.log('Registration was successful. Scope is ' + reg.scope);
+       }).catch(function(error) {
+       // registration failed
+       console.log('Registration failed with ' + error);
+      });
+    });
    }
